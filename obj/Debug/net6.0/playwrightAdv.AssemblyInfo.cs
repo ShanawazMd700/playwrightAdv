1074488f19eb7e6f9edd740a55a1fa6718250cd8
MD Shanawaz <shanawaz.md@i-raysolutions.com>
2025-10-06 17:11:04 +0530

    Second Commit

--- obj/Debug/net6.0/playwrightAdv.AssemblyInfo.cs
+++ obj/Debug/net6.0/playwrightAdv.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("playwrightAdv")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac6f53e83d6e953f0434a994bef02592344313f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("playwrightAdv")]
 [assembly: System.Reflection.AssemblyTitleAttribute("playwrightAdv")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
